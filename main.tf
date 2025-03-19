6f83942e7814d617275fda3c5f0d5308d0e7a04a
leyon.a.thampi <leothampi>
2025-03-19 15:46:57 +0530

    SDFSD

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "google" {
+  project = "dev-sec-ops-leon"  # Replace with your project ID
+  region  = "us-central1"
+}
+
+resource "google_compute_network" "cicd_network" {
+  name                    = "cicd-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "cicd_subnet" {
+  name          = "cicd-subnet"
+  ip_cidr_range = "10.0.2.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.cicd_network.id
+}
+
+# Provision a Compute Engine instance
+resource "google_compute_instance" "cicd_vm" {
+  name         = "cicd-vm"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"  # RedHat Enterprise Linux 8
+    }
+  }
+  
+  network_interface {
+    subnetwork = google_compute_subnetwork.cicd_subnet.id
+    access_config {}  # Assigns an external IP
+  }
+}
