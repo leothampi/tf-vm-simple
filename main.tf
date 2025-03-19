1fac2c1b4c94a599d8ba0af6c402e5a38ffd18d2
leyon.a.thampi <leothampi>
2025-03-19 18:20:46 +0530

    SDFSDsdf

--- main.tf
+++ main.tf
@@ -3,6 +3,20 @@ provider "google" {
   region  = "us-central1"
 }
 
+resource "google_storage_bucket" "tf_state" {
+  name     = "my-terraform-state-bucket"
+  location = "US-CENTRAL1"
+  versioning {
+    enabled = true  # Enable versioning for state recovery
+  }
+}
+terraform {
+  backend "gcs" {
+    bucket  = "my-terraform-state-bucket"
+    prefix  = "terraform/state"  # Optional: organizes state files in a folder
+  }
+}
+
 resource "google_compute_network" "cicd_network" {
   name                    = "cicd-network"
   auto_create_subnetworks = false
